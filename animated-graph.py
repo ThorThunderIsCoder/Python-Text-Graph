e9bdceb67abcf4047ef140bd13ae526f2d9efabf
Thor Helgeson <twhelgeson@gmail.com>
2020-07-15 16:38:08 -0400

    Initial Commit

---
+++ animated-graph.py
@@ -0,0 +1,93 @@
+import time, os, sys, curses, math, select
+
+rows, columns = os.popen('stty size', 'r').read().split()
+rows = int(rows)
+columns = int(columns)
+
+a = 10
+b = 0.1
+c = 0
+
+b_sign = -1
+b_upper = 0.117
+b_lower = 0
+b_increment = 0.0008
+
+b_frames = (b_upper - b_lower) / b_increment
+
+a_sign = -1
+a_upper = 13
+a_lower = 7
+a_increment = (a_upper - a_lower) / b_frames
+
+
+while True:
+	x = 0
+	y = 0
+	y_vals = []
+	string = "o"
+
+	for x in range(0, columns + 1):
+		y = a * math.sin(b*x + c) + 15
+
+		if(y <= rows):
+			y = int(round(y))
+			y_vals.append(y)
+
+	#print(y_vals)
+
+	decrement = 0
+	x_val = 0
+	graph = []
+	string = ""
+
+	for y in range(0, rows):
+		y = rows - decrement
+
+		for x in range(1, len(y_vals)):
+			if y_vals[x] == y:
+				string += "o"
+			else:
+				string += " "
+
+		graph.append(string)
+		string = ""
+		decrement += 1
+
+	blank_line = ""
+
+	for i in range(0, columns):
+		blank_line += " "
+
+	for l in graph:
+		print(l)
+
+	if b > b_upper:
+		b_sign = -1
+	elif b < b_lower:
+		b_sign = 1
+
+	b += b_sign * b_increment
+
+	a += b_sign * a_increment
+
+	c -= 0.1
+	time.sleep(0.04)
+
+
+
+'''
+graph_instruct = ""
+space_block = ""
+
+for l in graph:
+	for c in l:
+		if(c == " "):
+			space_block += c
+		else:
+			graph_instruct += 'sys.stdout.write("' + space_block + '")\n'
+			graph_instruct += 'sys.stdout.write("' + c + '")\n'
+			space_block = ""
+
+exec(graph_instruct)
+'''
---
+++ graph.py
@@ -0,0 +1,73 @@
+import time, os, sys, curses, math, select
+
+rows, columns = os.popen('stty size', 'r').read().split()
+rows = int(rows)
+columns = int(columns)
+
+x = 0
+y = 0
+y_vals = []
+
+string = "o"
+
+def largest(arr):
+	max = arr[0]
+
+	for i in range(1, len(arr)):
+		if(arr[i] > max):
+			max = arr[i]
+
+	return max
+
+for x in range(0, columns + 1):
+	y = 20 * math.sin(0.1*x) + 10
+
+	if(y <= rows):
+		y = int(round(y))
+		y_vals.append(y)
+
+#print(y_vals)
+
+decrement = 0
+x_val = 0
+graph = []
+string = ""
+
+for y in range(0, rows):
+	y = rows - decrement
+
+	for x in range(1, len(y_vals)):
+		if y_vals[x] == y:
+			string += "o"
+		else:
+			string += " "
+
+	graph.append(string)
+	string = ""
+	decrement += 1
+
+blank_line = ""
+
+for i in range(0, columns):
+	blank_line += " "
+
+for l in graph:
+	print(l)
+	if(l != blank_line):
+		time.sleep(0.01)
+
+'''
+graph_instruct = ""
+space_block = ""
+
+for l in graph:
+	for c in l:
+		if(c == " "):
+			space_block += c
+		else:
+			graph_instruct += 'sys.stdout.write("' + space_block + '")\n'
+			graph_instruct += 'sys.stdout.write("' + c + '")\n'
+			space_block = ""
+
+exec(graph_instruct)
+'''
